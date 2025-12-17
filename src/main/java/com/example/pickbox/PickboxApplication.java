04029c015d587672777dd9ab76e5fc52b2e75fc4
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2025-12-18 01:06:49 +0530

    Initial commit

---
+++ src/main/java/com/example/pickbox/PickboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pickbox/PickboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pickbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
