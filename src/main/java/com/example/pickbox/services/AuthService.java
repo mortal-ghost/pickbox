0546a0d8f4c0a6bde1e3bd46d5d2a14a7dba0dc0
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2025-12-20 00:50:35 +0530

    Implement authentication service logic

---
+++ src/main/java/com/example/pickbox/services/AuthService.java
@@ -0,0 +1,8 @@
+package com.example.pickbox.services;
+
+import com.example.pickbox.dtos.UserDto;
+
+public interface AuthService {
+    UserDto login(String email, String password);
+    UserDto register(String email, String password, String username);
+}
---
+++ src/main/java/com/example/pickbox/services/impl/AuthServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.pickbox.services.impl;
+
+import com.example.pickbox.dtos.UserDto;
+import com.example.pickbox.services.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+    public AuthServiceImpl() {
+    }
+
+    @Override
+    public UserDto login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public UserDto register(String email, String password, String username) {
+        return null;
+    }
+}
