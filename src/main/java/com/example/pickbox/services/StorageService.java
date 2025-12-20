db9b6610062e75704233ad337401d3fc8a69d688
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2025-12-20 23:08:53 +0530

    Add storage service

---
+++ src/main/java/com/example/pickbox/services/StorageService.java
@@ -0,0 +1,8 @@
+package com.example.pickbox.services;
+
+public interface StorageService {
+    String initiateUpload(String userId, String uploadId);
+    String generateSignedUrl(String uploadId, int chunkIndex);
+    void completeUpload(String uploadId);
+    void abortUpload(String uploadId);
+}
