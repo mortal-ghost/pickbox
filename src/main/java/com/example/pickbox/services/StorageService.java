fde7eac721e3b99725dcc08e38c283dfb088f777
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2025-12-21 21:20:55 +0530

    Implement file chunk upload for local storage

--- src/main/java/com/example/pickbox/services/StorageService.java
+++ src/main/java/com/example/pickbox/services/StorageService.java
@@ -2,7 +2,10 @@ package com.example.pickbox.services;
 
 public interface StorageService {
     String initiateUpload(String userId, String uploadId);
+
     String generateSignedUrl(String uploadId, int chunkIndex);
+
     void completeUpload(String uploadId);
+
     void abortUpload(String uploadId);
 }
--- src/main/java/com/example/pickbox/services/impl/LocalStorageService.java
+++ src/main/java/com/example/pickbox/services/impl/LocalStorageService.java
@@ -1,8 +1,11 @@
 package com.example.pickbox.services.impl;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
@@ -22,6 +25,18 @@ public class LocalStorageService implements StorageService {
     private final String baseUploadUrl;
 
     private static final String TEMP_CHUNK_DIR = ".tmp";
+
+    /**
+     * File Permissions: rw------- (600)
+     * Why:
+     * 1. Security: Only the owner (the application process) should be able to
+     * read/write the files.
+     * 2. Privacy: Prevents other users on the system from accessing sensitive user
+     * data.
+     * 3. Safety: No Execute (x) permission prevents execution of malicious scripts
+     * if uploaded.
+     */
+    private static final String FILE_PERMISSIONS_STRING = "rw-------";
 
     public LocalStorageService(@Value("${local.storage.path}") String uploadDirectory,
             @Value("${local.upload.url}") String baseUploadUrl) {
@@ -120,6 +135,16 @@ public class LocalStorageService implements StorageService {
             Files.deleteIfExists(tempChunkPath);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public void uploadChunk(String uploadId, int chunkIndex, InputStream inputStream) {
+        Path chunkPath = Path.of(uploadDirectory, TEMP_CHUNK_DIR + uploadId, String.valueOf(chunkIndex));
+        try (inputStream) {
+            Files.copy(inputStream, chunkPath, StandardCopyOption.REPLACE_EXISTING);
+            Files.setPosixFilePermissions(chunkPath, PosixFilePermissions.fromString(FILE_PERMISSIONS_STRING));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
