aca513405ab8ccc598e7a15b13017f524c521e86
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2025-12-22 01:03:12 +0530

    Implement APIs for folder creation and files fetch

--- src/main/java/com/example/pickbox/controllers/FileController.java
+++ src/main/java/com/example/pickbox/controllers/FileController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pickbox.dtos.ItemDto;
 import com.example.pickbox.dtos.UploadRequest;
+import com.example.pickbox.services.FileService;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,32 +20,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestAttribute;
 
-
 @RestController
 @RequestMapping("/files")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ItemDto>> getAllFilesAndFolders(@RequestAttribute(required = false) String userId) {
+        return ResponseEntity.ok(fileService.listFiles(null, userId));
+    }
+
+    private void validateUser(String userId) {
+        if (userId == null || userId.isEmpty()) {
+            throw new IllegalArgumentException("User ID is required");
+        }
+    }
+
     @PostMapping("/folder")
-    public ResponseEntity<ItemDto> createFolder(@RequestBody UploadRequest uploadRequest, 
-                                                @RequestAttribute(required = false) String userId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ItemDto> createFolder(@RequestBody UploadRequest uploadRequest,
+            @RequestAttribute(required = false) String userId) {
+        validateUser(userId);
+        return ResponseEntity
+                .ok(fileService.createFolder(uploadRequest.getName(), uploadRequest.getParentId(), userId));
     }
-    
+
     @GetMapping("/{id}")
-    public ResponseEntity<ItemDto> getFile(@PathVariable String id, 
-                                            @RequestAttribute(required = false) String userId) {
-        return ResponseEntity.ok().build();
-    }
-    
-    @GetMapping("/{id}/content")
-    public ResponseEntity<ItemDto> getFileContent(@PathVariable String id, 
-                                            @RequestAttribute(required = false) String userId,
-                                            HttpServletResponse response) {
+    public ResponseEntity<ItemDto> getFile(@PathVariable String id,
+            @RequestAttribute(required = false) String userId) {
+        validateUser(userId);
+        // TODO: Implement get single file
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("{id}/list")
-    public ResponseEntity<List<ItemDto>> listFiles(@PathVariable String id, 
-                                            @RequestAttribute(required = false) String userId) {
+    @GetMapping("/{id}/content")
+    public ResponseEntity<ItemDto> getFileContent(@PathVariable String id,
+            @RequestAttribute(required = false) String userId,
+            HttpServletResponse response) {
+        // TODO: Implement download
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping({ "/list", "/{id}/list" })
+    public ResponseEntity<List<ItemDto>> listFiles(@PathVariable(required = false) String id,
+            @RequestAttribute(required = false) String userId) {
+        return ResponseEntity.ok(fileService.listFiles(id, userId));
     }
 }
