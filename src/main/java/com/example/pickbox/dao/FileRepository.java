5011c55112fa2a88c01fc5f88e9aeffe6659b39e
Anmoldeep Singh <anmoldeepsingh148@gmail.com>
2026-01-06 23:46:17 +0530

    Fix bug

--- src/main/java/com/example/pickbox/dao/FileRepository.java
+++ src/main/java/com/example/pickbox/dao/FileRepository.java
@@ -2,14 +2,9 @@ package com.example.pickbox.dao;
 
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
 import com.example.pickbox.models.StorageItem;
-import org.springframework.lang.NonNull;
 
 public interface FileRepository extends MongoRepository<StorageItem, String> {
-    @Query("{ '_id' : ?0 }")
-    public boolean existsById(@NonNull String id);
 
     List<StorageItem> findAllByOwnerUserIdAndParentId(String ownerUserId, String parentId);
 
